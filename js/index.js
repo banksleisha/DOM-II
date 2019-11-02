6baaaf3ccffe13a36ffc6c83be4ee04cd2dce589
Leisha <banksleisha@yahoo.com>
2019-11-01 21:34:04 -0400

    add mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const doc = document;
+
+//Mouseover italic
+const header = doc.querySelector('.intro img');
+
+header.addEventListener('mouseover', (event) => {
+    event. stopPropagation();
+    event.target.style.fontWeight = 'italic';
+});
+
+//keydown listener
+
+doc.addEventListener('keydown', (event) => {
+    const keydown = event.key;
+    alert
+});
+
+
